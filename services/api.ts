a233fd561ebf27fa1b6f4da373b196a30f4449bf
rivohenintsoa <rivohenintsoaavotra@gmail.com>
2026-03-11 23:46:22 +0300

    feat: use environment variable

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
